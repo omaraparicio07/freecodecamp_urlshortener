34a2f81023590960ef545b2faeb3706fe0d5e274
System <none>
2017-02-18 18:57:45 +0000

    Initial commit from Gomix.
    Fix: App API and freeCodeCamp URLs (#4)
    
    This PR:
    - Changes `thread-paper.hyperdev.space` to `thread-paper.glitch.me`
    - Changes `forum.freecodecamp.com` to `freecodecamp.org/forum/`
    - Changes `freecodecamp.com` to `freecodecamp.org`
    - Closes https://github.com/freeCodeCamp/freeCodeCamp/issues/17550
    
    Updated npm dependencies and added a note on the compatabile version of mongoose for connecting to the free tier of mongodb atlas
    
    added .replit file
    added lockfile
    edited index to route to freeCodeCamp forum
    edited readme to have run on repl.it
    
    fix: change link to use solution api, update forum url
    
    Update Mongoose and Mongodb versions (#12)
    
    * Update Mongoose, Mongo, and Node versions
    
    feat: cleanup glitch assets and files and general prettifying
    
    feat: add dotenv
    
    feat: add and configure nodemon
    
    fix: html formatting and grammar
    
    fix: code highlighting, remove Roboto font
    
    fix: package.json spacing and .replit start script
    
    fix: refactor html and css
    
    fix: sync README.md user stories with pr #39311
    
    fix: remove user stories and example usage
    
    fix: add new favicon and clean up landing page
    
    fix: text in README.md
    
    fix: update title
    
    Co-authored-by: Shaun Hamilton <51722130+Sky020@users.noreply.github.com>
    
    Update public/style.css
    
    Co-authored-by: Shaun Hamilton <51722130+Sky020@users.noreply.github.com>
    
    Update public/style.css
    
    Co-authored-by: Shaun Hamilton <51722130+Sky020@users.noreply.github.com>
    
    Update public/style.css
    
    Co-authored-by: Shaun Hamilton <51722130+Sky020@users.noreply.github.com>
    
    Update public/style.css
    
    Co-authored-by: Shaun Hamilton <51722130+Sky020@users.noreply.github.com>
    
    Update public/style.css
    
    Co-authored-by: Shaun Hamilton <51722130+Sky020@users.noreply.github.com>
    
    fix: remove mongodb and mongoose
    
    Update server.js
    
    Update with es6
    
    Added Node.js documentation +Doc Improvements
    
    Adding Node.js documentation link for beginners, fix grammatical error, spacing and highlighting some keywords.
    
    Changed URL in the creation example to make it consistent with usage example.
    
    fix: Remove engine from package.json (#27)
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const express = require('express'),
+      mongo = require('mongodb'),
+      mongoose = require('mongoose'),
+      cors = require('cors'),
+      app = express();
+
+// Basic Configuration 
+const port = process.env.PORT || 3000;
+
+/** this project needs a db !! **/ 
+mongoose.connect(process.env.MONGOLAB_URI, { useMongoClient: true });
+
+app.use(cors());
+
+/* This project needs to parse POST bodies */
+// You should mount body-parser here
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
+});
